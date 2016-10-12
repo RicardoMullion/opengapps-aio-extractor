3eb746633622f85fad88202c840984b76bd0913c
bjsiu <bjsiu0617@gmail.com>
2016-10-12 16:22:27 +0800

    Update extract.sh

--- extract.sh
+++ extract.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 echo "start extract opengapps package"
-unzip *.zip -d org
-rm -rf tmp > /dev/null 2>&1
-mkdir -p tmp
-find . -name "*.tar.*" -exec tar -xf {} -C tmp/ \;
-rm -rf system > /dev/null 2>&1
-mkdir -p system
-for dir in tmp/*/
+unzip "gapps/*.zip" -d "gapps/pkg"
+rm -rf gapps/tmp > /dev/null 2>&1
+mkdir -p gapps/tmp
+find . -name "*.tar.xz" -exec tar -xf {} -C gapps/tmp/ \;
+rm -rf gapps/sys > /dev/null 2>&1
+mkdir -p gapps/sys
+for dir in gapps/tmp/*/
 do
   pkg=${dir%*/}
   dpi=$(ls -1 $pkg | head -1)
-
   echo "  - including $pkg/$dpi"
-  rsync -aq $pkg/$dpi/ system/
+  rsync -aq $pkg/$dpi/ gapps/sys/
 done
-rm -rf tmp
-rm -rf org
+# no leftovers
+rm -rf gapps/tmp
 echo "extract system folder done"
