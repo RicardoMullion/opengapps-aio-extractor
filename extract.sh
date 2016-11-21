05a413437d9a2739d42ba29207ef5c4bc28b352f
bjsiu <bjsiu0617@gmail.com>
2016-11-21 22:16:16 +0800

    Add files via upload

--- extract.sh
+++ extract.sh
@@ -4,14 +4,14 @@ unzip "gapps/*.zip" -d "gapps/pkg"
 rm -rf gapps/tmp > /dev/null 2>&1
 mkdir -p gapps/tmp
 find . -name "*.tar.[g|l|x]z" -exec tar -xf {} -C gapps/tmp/ \;
-rm -rf gapps/sys > /dev/null 2>&1
-mkdir -p gapps/sys
+rm -rf gapps/system > /dev/null 2>&1
+mkdir -p gapps/system
 for dir in gapps/tmp/*/
 do
   pkg=${dir%*/}
   dpi=$(ls -1 $pkg | head -1)
   echo "  - including $pkg/$dpi"
-  rsync -aq $pkg/$dpi/ gapps/sys/
+  rsync -aq $pkg/$dpi/ gapps/system/
 done
 # no leftovers
 rm -rf gapps/tmp
